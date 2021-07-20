148c0b279a5bcf235636d2f23eae1c01c8df0113
BangKiHyun <rlrlvh@naver.com>
2021-07-20 17:28:26 +0900

    feat: add WebConfig
    WebConfig 클래스를 통해 Web 모듈에 대한 Component Scan을 진행

---
+++ matcherloper-web/src/main/java/com/toy/matcherloper/web/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.toy.matcherloper.web.config;
+
+import com.toy.matcherloper.core.config.CoreConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @Import 어노테이션으로 외부 모듈의 Configuration 클래스를 가져와서 외부 모듈의 컴포넌트 스캔을 진행한다.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.toy.matcherloper.web")
+@Import(value = {CoreConfig.class})
+public class WebConfig {
+}
