9962b86996cbb0413640a156fe923dfa7c55658b
BangKiHyun <rlrlvh@naver.com>
2021-07-20 17:28:00 +0900

    feat: add CoreConfig
    CoreConfig 클래스를 통해 Core 모듈에 대한 Component Scan을 진행

---
+++ matcherloper-core/src/main/java/com/toy/matcherloper/core/config/CoreConfig.java
@@ -0,0 +1,9 @@
+package com.toy.matcherloper.core.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.toy.matcherloper.core")
+public class CoreConfig {
+}
