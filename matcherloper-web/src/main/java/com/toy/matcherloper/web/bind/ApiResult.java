b45f6e1cd0dc5cd3c9a25b0634b1e1e815a5e4b6
BangKiHyun <rlrlvh@naver.com>
2021-07-23 20:11:57 +0900

    feat(ApiResult.java): add ApiResult
    api 응답 형식 통일을 위한 클래스
    data: 응답할 body 값
    message: 응답 실패시 error message

---
+++ matcherloper-web/src/main/java/com/toy/matcherloper/web/bind/ApiResult.java
@@ -0,0 +1,28 @@
+package com.toy.matcherloper.web.bind;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ApiResult<T> {
+
+    private final T data;
+    private final String message;
+
+    public ApiResult(T data, String message) {
+        this.data = data;
+        this.message = message;
+    }
+
+    public static <T> ApiResult<T> succeed(T data) {
+        return new ApiResult<>(data, null);
+    }
+    public static <T> ApiResult<T> failed(Throwable throwable) {
+        return new ApiResult<>(null, throwable.getMessage());
+    }
+
+    public static <T> ApiResult<T> failed(String message) {
+        return new ApiResult<>(null, message);
+    }
+}
