2fb6976bc5093641bad1918daf5b05375979ba1e
jinmin <jinmin2216@naver.com>
2021-07-25 18:04:54 +0900

    feat: Modify UserRepository.java

--- matcherloper-core/src/main/java/com/toy/matcherloper/core/user/repository/UserRepository.java
+++ matcherloper-core/src/main/java/com/toy/matcherloper/core/user/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.toy.matcherloper.core.user.repository;
 import com.toy.matcherloper.core.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findUserByEmail(String email);
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserByEmailAndPassword(String email, String password);
 }
