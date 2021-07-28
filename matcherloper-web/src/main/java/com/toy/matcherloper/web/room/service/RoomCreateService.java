dbb24b7afe463419457de360faed02ab38ddac82
BangKiHyun <rlrlvh@naver.com>
2021-07-28 19:54:07 +0900

    refactor(CreateRoomRequest.java): change field from RoomPosition to RoomPositionDto

--- matcherloper-web/src/main/java/com/toy/matcherloper/web/room/service/RoomCreateService.java
+++ matcherloper-web/src/main/java/com/toy/matcherloper/web/room/service/RoomCreateService.java
@@ -1,8 +1,10 @@
 package com.toy.matcherloper.web.room.service;
 
 import com.toy.matcherloper.core.room.model.Room;
+import com.toy.matcherloper.core.room.model.RoomPosition;
 import com.toy.matcherloper.core.room.repository.RoomRepository;
 import com.toy.matcherloper.core.user.model.Owner;
+import com.toy.matcherloper.web.room.api.dto.RoomPositionDto;
 import com.toy.matcherloper.web.room.api.dto.request.CreateRoomRequest;
 import com.toy.matcherloper.web.room.exception.NotCreateRoomException;
 import com.toy.matcherloper.web.user.service.OwnerFindService;
@@ -10,7 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -23,7 +27,7 @@ public class RoomCreateService {
     public Long create(CreateRoomRequest request) {
         Owner owner = ownerFindService.findById(request.getOwnerId());
         checkOwnerHaveAnotherOpenRoom(owner);
-        final Room room = Room.create(owner, request.getRoomPositionList(), request.getName(),
+        final Room room = Room.create(owner, toPositionList(request.getRoomPositionList()), request.getName(),
                 request.getPossibleOfflineArea(), request.getRequiredUserNumber());
         roomRepository.save(room);
         return room.getId();
@@ -32,7 +36,13 @@ public class RoomCreateService {
     private void checkOwnerHaveAnotherOpenRoom(Owner owner) {
         final Optional<Room> room = roomRepository.findByOwner(owner);
         if (room.isPresent() && room.get().isOpen()) {
-            throw new NotCreateRoomException("owner already  have another open room");
+            throw new NotCreateRoomException("owner already have another open room");
         }
+    }
+
+    private List<RoomPosition> toPositionList(List<RoomPositionDto> roomPositionList) {
+        return roomPositionList.stream()
+                .map(dto -> new RoomPosition(dto.getPosition(), dto.isExist()))
+                .collect(Collectors.toList());
     }
 }
