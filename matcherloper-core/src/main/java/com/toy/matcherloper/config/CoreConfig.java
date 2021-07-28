037bc9139e719e087aad08e731886f6bbd593aab
BangKiHyun <rlrlvh@naver.com>
2021-07-28 18:09:49 +0900

    refactor(CoreConfig.java): Add Jpa & QueryDsl config in coreConfig

--- matcherloper-core/src/main/java/com/toy/matcherloper/config/CoreConfig.java
+++ matcherloper-core/src/main/java/com/toy/matcherloper/config/CoreConfig.java
@@ -1,9 +1,11 @@
-package com.toy.matcherloper.core.config;
+package com.toy.matcherloper.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = "com.toy.matcherloper.core")
+@Import(value = {JpaConfiguration.class, QueryDslConfiguration.class})
 public class CoreConfig {
 }
