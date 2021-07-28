70edeb566b00c186241a1995dd12d1cee44e2981
jinmin <jinmin2216@naver.com>
2021-07-28 19:33:44 +0900

    feat: Add UserNotFoundException.java

---
+++ matcherloper-web/src/main/java/com/toy/matcherloper/web/user/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.toy.matcherloper.web.user.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
