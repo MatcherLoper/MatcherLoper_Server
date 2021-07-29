a8e6bb171db0e43a09c73168137827101e3a92b5
jinmin <jinmin2216@naver.com>
2021-07-29 17:10:52 +0900

    refactor(UserDeleteService.java): Add service

--- matcherloper-web/src/main/java/com/toy/matcherloper/web/user/service/UserDeleteService.java
+++ matcherloper-web/src/main/java/com/toy/matcherloper/web/user/service/UserDeleteService.java
@@ -1,5 +1,6 @@
 package com.toy.matcherloper.web.user.service;
 
+import com.toy.matcherloper.core.user.model.User;
 import com.toy.matcherloper.core.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserDeleteService {
 
+    private final UserFindService userFindService;
     private final UserRepository userRepository;
 
     @Transactional
-    public void delete(Long userId) {
+    public Long delete(Long userId) {
+        User user = userFindService.findById(userId);
         userRepository.deleteById(userId);
+
+        return user.getId();
     }
 }
