4e04777c427077eb767d48db6e44ba38da1d1eff
BangKiHyun <rlrlvh@naver.com>
2021-08-02 18:47:28 +0900

    refactor(EventConfig.java): Add CoreConfig.class

--- matcherloper-event/src/main/java/com/toy/matcherloper/event/config/EventConfig.java
+++ matcherloper-event/src/main/java/com/toy/matcherloper/event/config/EventConfig.java
@@ -1,9 +1,12 @@
 package com.toy.matcherloper.event.config;
 
+import com.toy.matcherloper.config.CoreConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = "com.toy.matcherloper.event")
+@Import(value = {CoreConfig.class})
 public class EventConfig {
 }
