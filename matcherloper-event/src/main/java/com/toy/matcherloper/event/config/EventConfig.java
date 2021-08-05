67e7097a9715c5c63a2c3aff1edc1eaa1aa87d60
BangKiHyun <rlrlvh@naver.com>
2021-08-05 19:57:25 +0900

    feat: Add AsyncEventInitializer.java & register bean

--- matcherloper-event/src/main/java/com/toy/matcherloper/event/config/EventConfig.java
+++ matcherloper-event/src/main/java/com/toy/matcherloper/event/config/EventConfig.java
@@ -1,12 +1,20 @@
 package com.toy.matcherloper.event.config;
 
-import com.toy.matcherloper.config.CoreConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = "com.toy.matcherloper.event")
-@Import(value = {CoreConfig.class})
 public class EventConfig {
+
+    @Bean
+    public AsyncEventInitializer asyncEventConfig() {
+        return new AsyncEventInitializer();
+    }
+
+    @Bean
+    public EventResetProcessor eventResetProcessor() {
+        return new EventResetProcessor();
+    }
 }
--- matcherloper-event/src/main/java/com/toy/matcherloper/event/config/EventResetProcessor.java
+++ matcherloper-event/src/main/java/com/toy/matcherloper/event/config/EventResetProcessor.java
@@ -5,12 +5,10 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 
 @Aspect
 @Order(0)
-@Component
-public class EventResetConfig {
+public class EventResetProcessor {
     private ThreadLocal<Integer> nestedCount = ThreadLocal.withInitial(() -> 0);
 
     @Around("execution(public * com.toy.matcherloper..*Service.*(..))")
