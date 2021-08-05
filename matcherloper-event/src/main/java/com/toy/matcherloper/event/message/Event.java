681fbe5ccae4ec2171b87d834e65f9b133f6836a
BangKiHyun <rlrlvh@naver.com>
2021-08-05 19:56:23 +0900

    refactor(Event.java): Refactor constructor init timestamp

--- matcherloper-event/src/main/java/com/toy/matcherloper/event/message/Event.java
+++ matcherloper-event/src/main/java/com/toy/matcherloper/event/message/Event.java
@@ -3,8 +3,8 @@ package com.toy.matcherloper.event.message;
 public abstract class Event {
     private long timestamp;
 
-    public Event(long timestamp) {
-        this.timestamp = timestamp;
+    public Event() {
+        this.timestamp = System.currentTimeMillis();
     }
 
     public long getTimestamp() {
