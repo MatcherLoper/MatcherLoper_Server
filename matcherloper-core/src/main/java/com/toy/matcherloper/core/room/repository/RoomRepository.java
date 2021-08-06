b1e36c29826e6cf36cb2a110e95548c6d2db7c20
jinmin <jinmin2216@naver.com>
2021-08-06 20:02:09 +0900

    refactor: Modify RoomQueryRepository

--- matcherloper-core/src/main/java/com/toy/matcherloper/core/room/repository/RoomRepository.java
+++ matcherloper-core/src/main/java/com/toy/matcherloper/core/room/repository/RoomRepository.java
@@ -1,11 +1,12 @@
 package com.toy.matcherloper.core.room.repository;
 
 import com.toy.matcherloper.core.room.model.Room;
+import com.toy.matcherloper.core.room.repository.query.RoomQueryRepository;
 import com.toy.matcherloper.core.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long>, RoomQueryRepository {
     Optional<Room> findByUser(User user);
 }
--- matcherloper-core/src/main/java/com/toy/matcherloper/core/room/repository/query/RoomQueryRepository.java
+++ matcherloper-core/src/main/java/com/toy/matcherloper/core/room/repository/query/RoomQueryRepository.java
@@ -1,10 +1,9 @@
 package com.toy.matcherloper.core.room.repository.query;
 
 import com.toy.matcherloper.core.room.model.Room;
-import com.toy.matcherloper.core.user.model.Owner;
 
 import java.util.Optional;
 
 public interface RoomQueryRepository {
-    Optional<Room> findOneByOwner(Owner owner);
+    Optional<Room> findOpenedRoomByOwner(Long ownerId);
 }
