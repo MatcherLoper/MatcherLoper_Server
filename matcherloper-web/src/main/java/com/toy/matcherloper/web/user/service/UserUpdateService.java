c1c67010be0d47f37846bcedf9dd18d4b996c246
BangKiHyun <rlrlvh@naver.com>
2021-08-09 15:10:57 +0900

    refactor: Add dto converter

--- matcherloper-web/src/main/java/com/toy/matcherloper/web/user/service/UserUpdateService.java
+++ matcherloper-web/src/main/java/com/toy/matcherloper/web/user/service/UserUpdateService.java
@@ -1,23 +1,23 @@
 package com.toy.matcherloper.web.user.service;
 
-import com.toy.matcherloper.core.user.model.Address;
 import com.toy.matcherloper.core.user.model.Skill;
 import com.toy.matcherloper.core.user.model.User;
-import com.toy.matcherloper.core.user.model.UserPosition;
-import com.toy.matcherloper.web.user.api.dto.AddressDto;
+import com.toy.matcherloper.core.user.repository.SkillRepository;
 import com.toy.matcherloper.web.user.api.dto.request.UserUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
-import java.util.stream.Collectors;
+
+import static com.toy.matcherloper.web.utils.DtoConverter.*;
 
 @Service
 @RequiredArgsConstructor
 public class UserUpdateService {
 
     private final UserFindService userFindService;
+    private final SkillRepository skillRepository;
 
     @Transactional
     public Long update(Long userId, UserUpdateRequest userUpdateRequest) {
@@ -27,25 +27,16 @@ public class UserUpdateService {
                 userUpdateRequest.getName(),
                 userUpdateRequest.getPhoneNumber(),
                 userUpdateRequest.getIntroduction(),
-                toUserPositionSet(userUpdateRequest),
-                toSkillSet(userUpdateRequest),
+                toUserPositionSet(userUpdateRequest.getUserPositionDtoList()),
+                toSkillSet(userUpdateRequest.getSkillDtoList()),
                 toAddress(userUpdateRequest.getAddressDto()));
+        saveSkills(user.getSkillSet());
         return user.getId();
     }
 
-    private Set<UserPosition> toUserPositionSet(UserUpdateRequest userUpdateRequest) {
-        return userUpdateRequest.getUserPositionDtoList().stream()
-                .map(p -> new UserPosition(p.getType()))
-                .collect(Collectors.toSet());
-    }
-
-    private Set<Skill> toSkillSet(UserUpdateRequest userUpdateRequest) {
-        return userUpdateRequest.getSkillDtoList().stream()
-                .map(s -> new Skill(s.getName()))
-                .collect(Collectors.toSet());
-    }
-
-    private Address toAddress(AddressDto dto) {
-        return new Address(dto.getCity(), dto.getDetailed());
+    private void saveSkills(Set<Skill> skillSet) {
+        skillSet.stream()
+                .filter(skill -> !skillRepository.findByName(skill.getName()).isPresent())
+                .forEach(skillRepository::save);
     }
 }
