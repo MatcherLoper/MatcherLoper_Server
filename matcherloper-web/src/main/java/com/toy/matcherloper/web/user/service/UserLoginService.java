d7fc27ef098e0ead78ce92699c3b528b26b7c7de
BangKiHyun <rlrlvh@naver.com>
2021-08-09 15:10:57 +0900

    refactor: Add dto converter

--- matcherloper-web/src/main/java/com/toy/matcherloper/web/user/service/UserLoginService.java
+++ matcherloper-web/src/main/java/com/toy/matcherloper/web/user/service/UserLoginService.java
@@ -1,15 +1,10 @@
 package com.toy.matcherloper.web.user.service;
 
-import com.toy.matcherloper.core.user.repository.SkillRepository;
 import com.toy.matcherloper.core.user.exception.PasswordNotMatchedException;
-import com.toy.matcherloper.core.user.model.Address;
 import com.toy.matcherloper.core.user.model.Skill;
 import com.toy.matcherloper.core.user.model.User;
-import com.toy.matcherloper.core.user.model.UserPosition;
+import com.toy.matcherloper.core.user.repository.SkillRepository;
 import com.toy.matcherloper.core.user.repository.UserRepository;
-import com.toy.matcherloper.web.user.api.dto.AddressDto;
-import com.toy.matcherloper.web.user.api.dto.SkillDto;
-import com.toy.matcherloper.web.user.api.dto.UserPositionDto;
 import com.toy.matcherloper.web.user.api.dto.request.SignInRequest;
 import com.toy.matcherloper.web.user.api.dto.request.SignUpRequest;
 import com.toy.matcherloper.web.user.exception.EmailDuplicateException;
@@ -17,9 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
+
+import static com.toy.matcherloper.web.utils.DtoConverter.*;
 
 @Service
 @RequiredArgsConstructor
@@ -32,9 +27,14 @@ public class UserLoginService {
     @Transactional
     public Long signUp(SignUpRequest signUpRequest) {
         checkDuplicatedEmail(signUpRequest.getEmail());
-        User user = User.create(signUpRequest.getEmail(), signUpRequest.getPassword(), signUpRequest.getName(),
-                signUpRequest.getPhoneNumber(), signUpRequest.getIntroduction(), toUserPositionSet(signUpRequest.getUserPositionDtoList()),
-                toSkillSet(signUpRequest.getSkillDtoList()), toAddress(signUpRequest.getAddressDto()));
+        User user = User.create(signUpRequest.getEmail()
+                , signUpRequest.getPassword(),
+                signUpRequest.getName(),
+                signUpRequest.getPhoneNumber(),
+                signUpRequest.getIntroduction(),
+                toUserPositionSet(signUpRequest.getUserPositionDtoList()),
+                toSkillSet(signUpRequest.getSkillDtoList()),
+                toAddress(signUpRequest.getAddressDto()));
         saveSkills(user.getSkillSet());
         userRepository.save(user);
         return user.getId();
@@ -59,21 +59,5 @@ public class UserLoginService {
         skillSet.stream()
                 .filter(skill -> !skillRepository.findByName(skill.getName()).isPresent())
                 .forEach(skillRepository::save);
-    }
-
-    private Set<UserPosition> toUserPositionSet(List<UserPositionDto> userPositions) {
-        return userPositions.stream()
-                .map(p -> new UserPosition(p.getType()))
-                .collect(Collectors.toSet());
-    }
-
-    private Set<Skill> toSkillSet(List<SkillDto> skills) {
-        return skills.stream()
-                .map(s -> new Skill(s.getName()))
-                .collect(Collectors.toSet());
-    }
-
-    private Address toAddress(AddressDto addressDto) {
-        return new Address(addressDto.getCity(), addressDto.getDetailed());
     }
 }
