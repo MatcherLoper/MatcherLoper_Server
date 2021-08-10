339549525cd8d77d2686991af296aff65523b7f2
jinmin <jinmin2216@naver.com>
2021-08-10 18:35:31 +0900

    feat(RoomPositionRepository.java): Add RoomPositionRepository.java

---
+++ matcherloper-core/src/main/java/com/toy/matcherloper/core/room/repository/RoomPositionRepository.java
@@ -0,0 +1,7 @@
+package com.toy.matcherloper.core.room.repository;
+
+import com.toy.matcherloper.core.room.model.RoomPosition;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomPositionRepository extends JpaRepository<RoomPosition, Long> {
+}
