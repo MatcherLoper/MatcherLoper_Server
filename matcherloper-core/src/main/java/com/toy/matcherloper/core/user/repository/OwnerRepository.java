1b7b1b1c7d906031194513550e7d730108ae2897
BangKiHyun <rlrlvh@naver.com>
2021-08-11 15:27:28 +0900

    refactor: Delete owner repository & service

--- matcherloper-core/src/main/java/com/toy/matcherloper/core/user/repository/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.toy.matcherloper.core.user.repository;
-
-import com.toy.matcherloper.core.user.model.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwnerRepository extends JpaRepository<Owner, Long> {
-}
