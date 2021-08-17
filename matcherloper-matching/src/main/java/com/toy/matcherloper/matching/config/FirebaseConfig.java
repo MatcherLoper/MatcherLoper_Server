520006dbdd8fd776c15f57c891c245deb5d7eb34
BangKiHyun <rlrlvh@naver.com>
2021-08-17 19:03:26 +0900

    feat: Add firebase config

---
+++ matcherloper-matching/src/main/java/com/toy/matcherloper/matching/config/FirebaseConfig.java
@@ -0,0 +1,27 @@
+package com.toy.matcherloper.matching.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public GoogleCredentials googleCredentials() throws IOException {
+        String firebaseConfigPath = "firebase/firebase_service_key.json";
+        return GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+                .createScoped(Arrays.asList("https://www.googleapis.com/auth/cloud-platform"));
+    }
+
+    @Bean
+    public OkHttpClient okHttpClient() {
+        return new OkHttpClient();
+    }
+}
--- matcherloper-matching/src/main/java/com/toy/matcherloper/matching/config/MatchingConfig.java
+++ matcherloper-matching/src/main/java/com/toy/matcherloper/matching/config/MatchingConfig.java
@@ -2,8 +2,10 @@ package com.toy.matcherloper.matching.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = "com.toy.matcherloper.matching")
+@Import(value = {FirebaseConfig.class})
 public class MatchingConfig {
 }
