ed3c57c4759d67e84b516ef80d120920d19a4484
BangKiHyun <rlrlvh@naver.com>
2021-08-18 18:16:18 +0900

    fix(FirebaseConfig.java): Fix firebaseApp config

--- matcherloper-matching/src/main/java/com/toy/matcherloper/matching/config/FirebaseConfig.java
+++ matcherloper-matching/src/main/java/com/toy/matcherloper/matching/config/FirebaseConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -31,11 +30,12 @@ public class FirebaseConfig {
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         String firebaseConfigPath = "firebase/firebase_service_key.json";
-        FileInputStream refreshToken = new FileInputStream(firebaseConfigPath);
-
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                .setCredentials(GoogleCredentials
+                        .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+                        .createScoped(Arrays.asList("https://www.googleapis.com/auth/cloud-platform")))
                 .build();
+
 
         return FirebaseApp.initializeApp(options);
     }
