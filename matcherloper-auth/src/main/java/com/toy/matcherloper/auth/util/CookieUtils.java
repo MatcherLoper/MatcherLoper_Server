aef57180361cfcc0be5ad0d9e52f95258981ca6e
jinmin <jinmin2216@naver.com>
2021-08-18 15:49:17 +0900

    refactor(CookieUtils.java): Modify CookieUtils.java's indent

--- matcherloper-auth/src/main/java/com/toy/matcherloper/auth/util/CookieUtils.java
+++ matcherloper-auth/src/main/java/com/toy/matcherloper/auth/util/CookieUtils.java
@@ -5,22 +5,24 @@ import org.springframework.util.SerializationUtils;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Optional;
 
 public class CookieUtils {
     public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
         Cookie[] cookies = request.getCookies();
+        checkCookieIsNullOrEmpty(cookies);
 
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(name))
+                .findFirst();
+    }
+
+    private static void checkCookieIsNullOrEmpty(Cookie[] cookies) {
         if (cookies != null && cookies.length > 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(name)) {
-                    return Optional.of(cookie);
-                }
-            }
+            throw new IllegalArgumentException("Cookie's null or empty !!");
         }
-
-        return Optional.empty();
     }
 
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
@@ -33,17 +35,15 @@ public class CookieUtils {
 
     public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
         Cookie[] cookies = request.getCookies();
-
-        if (cookies != null && cookies.length > 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(name)) {
+        checkCookieIsNullOrEmpty(cookies);
+        Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(name))
+                .forEach(cookie -> {
                     cookie.setPath("/");
                     cookie.setValue("");
                     cookie.setMaxAge(0);
                     response.addCookie(cookie);
-                }
-            }
-        }
+                });
     }
 
     public static String serialize(Object obj) {
