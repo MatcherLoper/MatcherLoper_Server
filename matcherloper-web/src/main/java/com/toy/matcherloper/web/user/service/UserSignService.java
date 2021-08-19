8c15d62cd16cb3388b407ff0e9faf5be3a1388e1
jinmin <jinmin2216@naver.com>
2021-08-19 16:43:15 +0900

    refactor(UserSignService.java): Modify UserSignService.java

--- matcherloper-web/src/main/java/com/toy/matcherloper/web/user/service/UserSignService.java
+++ matcherloper-web/src/main/java/com/toy/matcherloper/web/user/service/UserSignService.java
@@ -1,21 +1,14 @@
 package com.toy.matcherloper.web.user.service;
 
-import com.toy.matcherloper.auth.jwt.JwtTokenProvider;
 import com.toy.matcherloper.core.user.exception.PasswordNotMatchedException;
 import com.toy.matcherloper.core.user.model.User;
 import com.toy.matcherloper.core.user.model.type.AuthProviderType;
-import com.toy.matcherloper.core.user.repository.SkillRepository;
-import com.toy.matcherloper.core.user.repository.UserPositionRepository;
 import com.toy.matcherloper.core.user.repository.UserRepository;
 import com.toy.matcherloper.web.user.api.dto.request.SignInRequest;
 import com.toy.matcherloper.web.user.api.dto.request.SignUpRequest;
 import com.toy.matcherloper.web.user.api.dto.response.SignInResponse;
 import com.toy.matcherloper.web.user.exception.EmailDuplicateException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,11 +21,7 @@ public class UserSignService {
 
     private final UserRepository userRepository;
     private final UserFindService userFindService;
-    private final SkillRepository skillRepository;
-    private final UserPositionRepository userPositionRepository;
     private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
     public Long signUp(SignUpRequest signUpRequest) {
@@ -46,8 +35,6 @@ public class UserSignService {
                 toSkillSet(signUpRequest.getSkillDtoList()),
                 toAddress(signUpRequest.getAddressDto()),
                 AuthProviderType.local);
-        saveSkills(user.getSkillSet());
-        userPositionRepository.saveAll(user.getUserPositionSet());
         userRepository.save(user);
         return user.getId();
     }
@@ -55,22 +42,14 @@ public class UserSignService {
     @Transactional(readOnly = true)
     public SignInResponse signIn(SignInRequest signInRequest) {
         User user = userFindService.findByEmail(signInRequest.getEmail());
+        checkMatchedPassword(signInRequest.getPassword(), user.getPassword());
+        return new SignInResponse(user.getId());
+    }
 
-        if (!passwordEncoder.matches(signInRequest.getPassword(), user.getPassword())) {
+    private void checkMatchedPassword(String requestPassword, String userPassword) {
+        if (!passwordEncoder.matches(requestPassword, userPassword)) {
             throw new PasswordNotMatchedException("Password is not matched!");
         }
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        signInRequest.getEmail(),
-                        signInRequest.getPassword()
-                )
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = jwtTokenProvider.createToken(authentication);
-
-        return new SignInResponse(user.getId(), token);
     }
 
     private void checkDuplicatedEmail(String email) {
