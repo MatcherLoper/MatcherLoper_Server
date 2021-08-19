eb20701810bf787282f837f7308fe682c0cda553
jinmin <jinmin2216@naver.com>
2021-08-19 16:37:14 +0900

    fix(CookieUtils.java): Fix CookieUtils.java method logic

--- matcherloper-auth/src/main/java/com/toy/matcherloper/auth/util/CookieUtils.java
+++ matcherloper-auth/src/main/java/com/toy/matcherloper/auth/util/CookieUtils.java
@@ -20,7 +20,7 @@ public class CookieUtils {
     }
 
     private static void checkCookieIsNullOrEmpty(Cookie[] cookies) {
-        if (cookies != null && cookies.length > 0) {
+        if (!(cookies != null && cookies.length > 0)) {
             throw new IllegalArgumentException("Cookie's null or empty !!");
         }
     }
